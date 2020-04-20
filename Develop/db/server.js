316146da5f31f32791a3255ddc2d67983841b267
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-04-20 16:21:46 -0400

    fixed script links in index.html and notes.js. added accsess to static files.

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -8,14 +8,20 @@ const PORT = 7500;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"../","public")))
 
 //sends notes.html when /notes is requested
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname,"../","public","notes.html"))
 })
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname,"db.json"))
+})
+
 //if anything except the app.get above is requested, sends index.html
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname,"../","public", "index.html"))
+   
 })
 
 //port listener
