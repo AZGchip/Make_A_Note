a5a7d4af7ffa76e88fa6bb8a3781d182ae9ed1db
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-04-25 14:38:26 -0400

    added dynamic port

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -3,8 +3,7 @@ const express = require("express");
 const fs = require("fs")
 const app = express();
 const path = require("path");
-//PORT 
-const PORT = 7500;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
